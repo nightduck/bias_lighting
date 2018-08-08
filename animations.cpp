89d90864c9c32f8e6b4e8c2d7df9b02ebba32024
oren <virtualoren@gmail.com>
2018-08-07 19:58:00 -0500

    Added references to future flash animation (just structural, nothing functional yet)

--- animations.cpp
+++ animations.cpp
@@ -38,17 +38,29 @@ static PyObject* wipe_ani(PyObject* self, PyObject* args) {
     return nullptr;
 }
 
+static PyObject* flash_ani(PyObject* self, PyObject* args) {
+    PyObject* strip;
+    int* position;
+    PyListObject* swaths;
+
+    int status = PyArg_ParseTuple(args, "OiO;flash_ani - couldn't parse args", strip, position, swaths);
+
+    return nullptr;
+}
+
 // ---------------------Python docs----------------------------------
 char* animations_doc = "animations(): TODO - Write a doc";
 char* pong_ani_doc = "pong_ani(): TODO - Write a doc";
 char* ember_ani_doc = "ember_ani(): TODO - Write a doc";
 char* wipe_ani_doc = "wipe_ani(): TODO - Write a doc";
+char* flash_ani_doc = "flash_ani(): TODO - Write a doc";
 
 // ---------------------Python method defs---------------------------
 static PyMethodDef animations_methods[] = {
         {"pong_ani", (PyCFunction)pong_ani, METH_VARARGS, pong_ani_doc},
         {"ember_ani", (PyCFunction)ember_ani, METH_VARARGS, ember_ani_doc},
         {"wipe_ani", (PyCFunction)wipe_ani, METH_VARARGS, wipe_ani_doc},
+        {"flash_ani", (PyCFunction)flash_ani, METH_VARARGS, flash_ani_doc},
         {NULL}
 };
 
